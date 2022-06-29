2ceb49ec4c2c4a1cf90a99b1eb7e0f8bc711d471
namsi-elikunda <namsi.elikunda@turnkeyafrica.com>
2022-06-29 17:56:18 +0300

    viewmodel addition

--- app/src/main/java/com/example/weatherapp/ui/Weatherscreen.kt
+++ app/src/main/java/com/example/weatherapp/ui/Weatherscreen.kt
@@ -1,12 +1,15 @@
 package com.example.weatherapp
 
+import androidx.activity.compose.LocalActivityResultRegistryOwner.current
+import androidx.activity.result.ActivityResultRegistryOwner
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -17,13 +20,15 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.weatherapp.service.dto.CurrentWeather
+import com.example.weatherapp.ui.WeatherviewModel
 import com.example.weatherapp.ui.theme.WeatherappTheme
-import kotlinx.coroutines.launch
 
 
-@OptIn(ExperimentalMaterialApi::class)
+
 @Composable
-fun Weatherscreen() {
+fun Weatherscreen(viewModel:WeatherviewModel) {
+    val current by viewModel.current.collectAsState(null)
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -33,12 +38,15 @@ fun Weatherscreen() {
     ) {
         Card(
             modifier = Modifier
-                .width(350.dp)
+                .width(550.dp)
                 .height(500.dp),
             shape = RoundedCornerShape(16.dp),
-            backgroundColor = Color.White,
-            elevation = 10.dp
+            elevation = 10.dp,
         ) {
+            Image(
+                painter = painterResource(id = R.drawable.bluebackground),
+                contentDescription = "Background", contentScale = ContentScale.FillHeight
+            )
             Column(
                 modifier = Modifier.fillMaxSize(),
                 verticalArrangement = Arrangement.Top,
@@ -54,12 +62,13 @@ fun Weatherscreen() {
                     modifier = Modifier.fillMaxSize(),
                     verticalAlignment = Alignment.CenterVertically,
                     horizontalArrangement = Arrangement.SpaceEvenly,
-
                     ) {
+                    current?.let {
+                        // Weather(weather =it)
+                    }
                 }
             }
         }
-        Divider()
         Column(
             modifier = Modifier
                 .fillMaxSize()
@@ -67,14 +76,19 @@ fun Weatherscreen() {
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.SpaceBetween
         ) {
+            Divider()
             Card(
                 modifier = Modifier
-                    .width(350.dp)
+                    .width(550.dp)
                     .height(280.dp),
                 shape = RoundedCornerShape(16.dp),
-                backgroundColor = Color.White,
                 elevation = 10.dp
             ) {
+                Image(
+                    painter = painterResource(id = R.drawable.blackbackground),
+                    contentDescription = "black back",
+                    contentScale = ContentScale.FillHeight
+                )
                 Column(
                     modifier = Modifier.fillMaxSize(),
                     verticalArrangement = Arrangement.Top,
@@ -99,24 +113,24 @@ fun Weatherscreen() {
 @Composable
 fun Weather(weather: CurrentWeather) {
     Box {
-        Image(
-            painter = painterResource(id = R.drawable.bluebackground),
-            contentDescription = "Background",
-            modifier = Modifier.fillMaxWidth(),
-            contentScale = ContentScale.FillWidth,
-        )
         Column(
             Modifier
                 .padding(top = 48.dp)
                 .align(Alignment.TopCenter),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(text = "Wind")
-            Text(text = "Humidity")
-            Text(text = "Precipitation")
+            Text(text = Temperature(weather.main.temp), fontSize = 48.sp, color = Color.White)
+            Text(text = weather.weather.first().main, fontSize = 28.sp, color = Color.White)
+            Text(text = weather.name, fontSize = 18.sp, color = Color.White)
+            Text(text = weather.name, fontSize = 18.sp, color = Color.White)
+
 
         }
     }
+
+}
+@Composable
+fun Temperature(temp: Double): String {
 
 }
 
