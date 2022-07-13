0dce23f0e722be6fc0104be6f9363dafe6f93762
Orinda Harrison <harrison@regulusweb.com>
2022-07-14 01:12:39 +0300

    WIP - state error fixes

--- app/src/main/java/com/example/weatherapp/ui/ui/WeatherviewModel.kt
+++ app/src/main/java/com/example/weatherapp/ui/ui/WeatherviewModel.kt
@@ -1,19 +1,40 @@
 package com.example.weatherapp.ui.ui
 
 import android.annotation.SuppressLint
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.weatherapp.service.WeatherRepository
-import com.example.weatherapp.service.dto.CurrentWeather
 import com.example.weatherapp.service.dto.FullWeather
+import com.google.android.gms.maps.model.LatLng
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
 import javax.inject.Inject
+
 @HiltViewModel
 @SuppressLint("MissingPermission")
-class WeatherviewModel @Inject constructor(repository: WeatherRepository) :ViewModel() {
-    val current: Flow<CurrentWeather> =repository.getCurrentWeather()
-    val forecast: Flow<FullWeather> =repository.getcurrentDailyForecast()
+class WeatherviewModel @Inject constructor(repository: WeatherRepository) : ViewModel() {
+    private val location = MutableSharedFlow<LatLng>()
 
+    val current = location.transformLatest { location ->
+        val flow = repository.getCurrentWeather(location).onStart {
+            Log.i(TAG, "Loading weather updates for $location...")
+        }
+        emitAll(flow)
+    }.catch {
+        Log.e(TAG, "Error : ${it.localizedMessage}", it)
+    }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(), null)
+    val forecast: Flow<FullWeather> = repository.getcurrentDailyForecast()
+
+    fun setLocation(location: LatLng) {
+        viewModelScope.launch {
+            this@WeatherviewModel.location.emit(location)
+        }
+    }
+
+    private companion object {
+        const val TAG = "WeatherviewModel"
+    }
 }
 
